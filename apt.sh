df703d63343c33bb507785ea8548fb9cbb7d5385
Simon Selg <simon@selg.me>
2016-07-16 15:26:00 +0200

    fix(apt): dont promt for confirmation

--- apt.sh
+++ apt.sh
@@ -4,24 +4,24 @@ sudo apt-get update
 sudo apt-get upgrade
 
 # first things first
-sudo apt-get install subversion git
+sudo apt-get install -y subversion git
 
 # insatll node
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # for node-gylp
-sudo apt-get install build-essential
+sudo apt-get install -y build-essential
 
 # for ls and cat
-sudo apt-get install pygmentize
+sudo apt-get install -y pygmentize
 
 # fish
-sudo apt-get install fish
+sudo apt-get install -y fish
 
 # generic colouriser  http://kassiopeia.juls.savba.sk/~garabik/software/grc/
-sudo apt-get install grc
+sudo apt-get install -y grc
 
 # install thefuck
-sudo apt install python3-dev python3-pip
+sudo apt install -y python3-dev python3-pip
 sudo -H pip3 install thefuck
